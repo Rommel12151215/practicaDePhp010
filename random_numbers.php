b25343086f58dcb5044afb42531170914eecb99b
Rommel <karlsrommel255@gmail.com>
2023-12-01 14:04:53 -0600

    practicaDePhp010

---
+++ random_numbers.php
@@ -0,0 +1,9 @@
+<?php
+$min = 1;
+$max = 100;
+
+// Generar un número aleatorio entre $min y $max
+$random_number = rand($min, $max);
+
+echo "Número aleatorio entre $min y $max: $random_number";
+?>
